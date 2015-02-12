2ab1f4edf2d61548404a95fa6181cca55be0b229
Daniel Chang <daniel.chang85@gmail.com>
2015-02-12 12:35:35 -0500

    Static site init

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function displayCohorts(responseCohorts){
+
+  console.log(responseCohorts);
+
+  var cohorts = $('#cohorts');
+
+  $.each(responseCohorts.cohorts, function(_, cohort){
+    cohorts.append('<li data-cohort-id='+cohort.id+'>'+cohort.name+'</li>');
+  });
+}
