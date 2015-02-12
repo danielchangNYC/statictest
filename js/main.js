67ae2d23e60c06e5537036d60b8d89e9266edf75
Daniel Chang <daniel.chang85@gmail.com>
2015-02-12 14:42:13 -0500

    Add handlebars and bootstrap. Display cohort info

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+
+
 function displayCohorts(responseCohorts){
 
   console.log(responseCohorts);
 
-  var cohorts = $('#cohorts');
+  var cohorts = $('#cohorts'),
+    cohortTemplate = Handlebars.compile($("#cohort_template").html());
 
   $.each(responseCohorts.cohorts, function(_, cohort){
-    cohorts.append('<li data-cohort-id='+cohort.id+'>'+cohort.name+' MAX SEATS: '+cohort.max_enrollments+'</li>');
+    var context = cohort;
+    context.fullText = cohort.full ? 'Open' : 'Closed';
+    cohorts.append(cohortTemplate(context));
   });
 }
