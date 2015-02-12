6e820abaeefdaaaa9937950c3601af52526a3464
Daniel Chang <daniel.chang85@gmail.com>
2015-02-12 14:15:32 -0500

    Show max enrollments

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ function displayCohorts(responseCohorts){
   var cohorts = $('#cohorts');
 
   $.each(responseCohorts.cohorts, function(_, cohort){
-    cohorts.append('<li data-cohort-id='+cohort.id+'>'+cohort.name+'</li>');
+    cohorts.append('<li data-cohort-id='+cohort.id+'>'+cohort.name+' MAX SEATS: '+cohort.max_enrollments+'</li>');
   });
 }
