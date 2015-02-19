59084b76e4e8d3e9df3dcbe21fe9823f47db82a8
vanessadean <vanessa.dean@gmail.com>
2015-02-19 14:31:08 -0500

    updated ajaz

--- js/course_select.js
+++ js/course_select.js
@@ -56,7 +56,7 @@ APP.Multiform.prototype.addEnrollButtonListener = function ($div) {
     });
     // send dataResponse to //enroll
     $.ajax({
-        url: 'http://50df9cfc.ngrok.com/api/carts',
+        url: 'http://497ca91.ngrok.com/api/carts',
         type: 'POST',
         contentType: 'application/json',
         crossDomain: true,
@@ -66,7 +66,8 @@ APP.Multiform.prototype.addEnrollButtonListener = function ($div) {
           console.log(res["cart"]["id"]);
           var cart_id = res["cart"]["id"];
           // redirect to this window.location redirect customers/new
-          window.location.href='http://localhost:3000/customers/new?cart_id='+cart_id;
+          
+          window.location.href='http://localhost:3000/carts/'+cart_id+'/customers/new';
         }
       });
   });
